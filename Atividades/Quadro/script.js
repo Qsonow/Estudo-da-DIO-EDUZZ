3d005469ec2520acc0e67a43f0a699caafbeeab7
Luan Chaves Barbosa <70718326+Qsonow@users.noreply.github.com>
2021-11-30 21:44:30 -0300

    genius

---
+++ Atividades/Quadro/script.js
@@ -0,0 +1,14 @@
+// let num1 = 25;
+// let num2 = 27;
+
+// const boi = n => new Promise((resolve, reject) =>{
+//     boi(n, (err, contents) => {
+//         if (err){
+//             reject(err)
+//         }else {
+//             resolve(contents)
+//         }
+//     }
+//     ) 
+// })
+// boi(num1);
